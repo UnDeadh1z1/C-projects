163d457d6b60a8ea69ba5b5dbf5c67b2574b6032
UnDeadh1z1 <bss2111@yandex.ru>
2021-12-29 17:28:08 +0300

    unit testing on C#
    unit testing on C#

---
+++ Модульное тестирование c#/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using static ConsoleApp1.moduleAttribute;
+namespace ConsoleApp1
+{
+    class Program
+    {//Функция ввода размера вектора
+       
+        static void Main(string[] args)
+        {
+            
+            int n = size_v();
+
+            float[] x = new float[n];
+            write_v(x,"x");
+            sort_v_Shell(x);
+
+            float[] y = new float[n];
+            write_v(y,"y");
+            sort_v_Shell(y);
+
+            float[] z = new float[n];
+            write_v(z,"z");
+            sort_v_Shell(z);
+            //задания матрицы
+            float[,] A = new float[3, x.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                A[0,i] = x[i];
+            }
+            for (int i = 0; i < y.Length; i++)
+            {
+                A[1,i] = y[i];
+
+            }
+            for (int i = 0; i < z.Length; i++)
+            {
+                A[2,i] = z[i];
+
+            }
+            //вывод матрицы
+            string s = "";
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    s += string.Format(" {0:f}    ", A[i, j]);
+                }
+                s += "\n";
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ Модульное тестирование c#/ConsoleApp1/module.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class moduleAttribute : Attribute
+    {
+       public static int size_v()
+        {
+            int x = -1;
+            while (x > 7 || x < 2)
+            {
+                try
+                {
+
+                    Console.WriteLine("ВВЕДИТЕ РАЗМЕР ВЕКТОРОВ");
+                    x = int.Parse(Console.ReadLine());
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("ВВЕДЕНО НЕКОРРЕКТНОЕ ЗНАЧЕНИЕ");
+                }
+            }
+            return x;
+        }
+        //Функция ввода значения вектора 
+        public static void write_v(float[] v, string s)
+        {
+            int i = 0;
+            while (i < v.Length)
+            {
+                try
+                {
+                    Console.WriteLine(string.Format("ВВЕДИТЕ ЗНАЧЕНИЕ {0:d} ВЕКТОРА {1:s}", i + 1, s));
+                    v[i] = float.Parse(Console.ReadLine());
+                    while( v[i]  > 100.0 || v[i] < -100.0){
+                        Console.WriteLine(string.Format("ВВЕДИТЕ ЗНАЧЕНИЕ {0:d} ВЕКТОРА {1:s}", i + 1, s));
+                        v[i] = float.Parse(Console.ReadLine());
+                    }
+                    i++;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("ВВЕДЕНО НЕКОРРЕКТНОЕ ЗНАЧЕНИЕ");
+                }
+            }
+        }
+        //функция сортировки вектора методом Шелла
+        public static void sort_v_Shell(float[] v)
+        {
+            int d = v.Length;
+            d = d / 2;
+            while (d > 0)
+            {
+                for (int i = 0; i < v.Length - d; i++)
+                {
+                    int j = i;
+                    while (j >= 0 && v[j] > v[j + d])
+                    {
+                        float count = v[j];
+                        v[j] = v[j + d];
+                        v[j + d] = count;
+                        j--;
+                    }
+                }
+                d = d / 2;
+            }
+        }
+    }
+}
